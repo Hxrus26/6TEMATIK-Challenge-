b2da0ef6c4c0f4fe3adae5c8f03c1223ef449f1b
Hxrus26 <yanis2101@live.fr>
2022-01-19 00:57:18 +0100

    first front-end block

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,67 @@
     <title>6TEMATIK</title>
 </head>
 <body>
-    <h1>Welcome</h1>
-    <script src="/public/js/app.js"></script>
+
+  <!-- Navbar -->
+
+<ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link" href="#">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Cars</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Contact</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">About us</a>
+  </li>
+</ul>
+
+<!-- Cards for Back display -->
+
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Tesla Model S P85D</h5>
+    <p class="card-text">Quick descriptions for this model</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Toyota Supra MK4</h5>
+    <p class="card-text">Quick descriptions for this model</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Mazda MX-5 NA</h5>
+    <p class="card-text">Quick descriptions for this model</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+<!-- Pagination -->
+
+<nav aria-label="...">
+  <ul class="pagination pagination-lg">
+  <ul class="pagination justify-content-center">
+    <li class="page-item disabled">
+      <a class="page-link" href="#" tabindex="-1">1</a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+  </ul>
+</nav> 
+
+  <script src="/public/js/app.js"></script>
+
 </body>
 </html>
